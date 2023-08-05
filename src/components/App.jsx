731fd4a9f773b28e6b2f785f16ecccd65706ecdb
AlexGoIT <alexandrpuryaev@gmail.com>
2023-08-05 09:52:32 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,3 @@
-import { useSelector } from 'react-redux';
-import { getContacts, getFilter } from 'redux/selectors';
-
 import Container from '@mui/material/Container';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
@@ -8,26 +5,13 @@ import Filter from './Filter';
 import { Title, ListTitle } from './App.styled';
 
 const App = () => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
-  console.log('App contacts', contacts);
-  // console.log('App filter', filter);
-
-  // Filter
-  const contactFilter = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase().trim())
-    );
-  };
-
   return (
     <Container className="container" maxWidth="sm" sx={{ mt: 2 }}>
       <Title>Phonebook</Title>
       <ContactForm />
       <ListTitle>Contacts</ListTitle>
-      <Filter filter={filter} />
-      <ContactList contacts={contactFilter(contacts)} />
+      <Filter />
+      <ContactList />
     </Container>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,22 @@
-import PropTypes from 'prop-types';
 import { List } from './ContactList.styled';
 import ContactItem from '../ContactItem';
+import { useSelector } from 'react-redux';
+import { getContacts, getFilter } from 'redux/selectors';
 
-const ContactList = ({ contacts }) => {
+const ContactList = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  // Filter
+  const contactFilter = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase().trim())
+    );
+  };
+
   return (
     <List>
-      {contacts.map(contact => {
+      {contactFilter().map(contact => {
         return <ContactItem key={contact.id} contact={contact} />;
       })}
     </List>
@@ -13,13 +24,3 @@ const ContactList = ({ contacts }) => {
 };
 
 export default ContactList;
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-};
